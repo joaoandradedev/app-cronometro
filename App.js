b780212243af5ac3b3e6902c57bf07b39350c520
João Vitor <joaovitor2004andrade@gmail.com>
2024-03-03 13:38:22 -0300

    app.js

--- App.js
+++ App.js
@@ -1,20 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-export default function App() {
-  return (
+let timer = null;
+let ss = 0;
+let mm = 0;
+let hh = 0;
+
+export default function App(){
+  const [numero, setNumero] = useState('00:00:00');
+  const [botao, setBotao] = useState('INICIAR');
+  const [ultimo, setUltimo] = useState(null);
+
+  function iniciar(){
+    //Aqui vai parar o timer
+    if(timer !== null){
+      clearInterval(timer);
+      timer = null;
+
+      setBotao('INICIAR');
+    }else{
+      //Começar a girar o timer
+      timer = setInterval( () => {
+        ss++;
+
+        if(ss == 60){
+          ss = 0;
+          mm++;
+        }
+
+        if(mm == 60){
+          mm = 0;
+          hh++;
+        }
+
+        let format = 
+        (hh < 10 ? '0' + hh : hh) + ':'
+        + (mm < 10 ? '0' + mm : mm) + ':'
+        + (ss < 10 ? '0' + ss : ss)
+
+        setNumero(format);
+
+      }, 1000);
+      setBotao('PARAR');
+    }
+  }
+
+  function reiniciar(){
+    if(timer !== null){
+      //Parar o timer
+      clearInterval(timer);
+      timer = null;
+    }
+
+    setUltimo(numero);
+    setNumero('00:00:00');
+    ss = 0;
+    mm = 0;
+    hh = 0;
+
+    setBotao('INICIAR');
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Image
+        source={require('./src/crono.png')}
+      />
+
+        <Text style={styles.timer}> {numero} </Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn} onPress={iniciar}>
+          <Text style={styles.btnTexto}> {botao} </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={reiniciar}>
+          <Text style={styles.btnTexto}>REINICIAR</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.areaUltima}>
+        <Text style={styles.textoCorrida}>
+         { ultimo ? 'Último tempo: ' + ultimo : '' }
+        </Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  container:{
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00aeef'
   },
-});
+  timer:{
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 40,
+  },
+  btn:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  },
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  },
+  areaUltima:{
+    marginTop: 40
+  },
+  textoCorrida:{
+    fontSize: 23,
+    color: '#fff',
+    fontStyle: 'italic'
+  }
+})
